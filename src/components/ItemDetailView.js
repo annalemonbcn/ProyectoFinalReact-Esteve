edf64b293019664efdacddd5ad3ee4974516319c
Anna Esteve <aesteve8@gmail.com>
2023-08-21 12:59:09 +0200

    itemDetaiList changed

--- src/components/ItemDetailView.js
+++ src/components/ItemDetailView.js
@@ -3,31 +3,33 @@ import Cart from "./svg/Cart";
 
 const ItemDetailView = ({ data }) => {
   return (
-    <div>
+    <>
       {data.length === 0 ? (
         <p>Cargando</p>
       ) : (
         <div>
-          <h2 className="text-xl lg:text-2xl text-gray-600 font-semibold mt-6 lg:mt-0">{data.title}</h2>
+          <h2 className="text-xl lg:text-2xl text-gray-600 font-semibold mt-6 lg:mt-0">
+            {data.title}
+          </h2>
           <div className="flex flex-col lg:flex-nowrap lg:flex-row gap-5 mt-8 lg:mt-6">
-            <div className="bg-soft-grey w-full h-[500px] p-5 box-border">
+            <div className="bg-soft-grey lg:w-2/5 h-[500px] p-5 box-border">
               <img
                 className="max-h-full mx-auto relative top-1/2 transform -translate-y-1/2"
                 src={data.image}
                 alt={data.title}
               />
             </div>
-            <div className="bg-soft-grey w-full h-[500px] p-5 box-border">
+            <div className="bg-soft-grey lg:w-2/5 h-[500px] p-5 box-border">
               <img
                 className="max-h-full mx-auto relative top-1/2 transform -translate-y-1/2"
                 src={data.image}
                 alt={data.title}
               />
             </div>
-            <div className="pt-2 lg:p-0" id="productInfo">
+            <div className="pt-2 lg:p-0 lg:w-1/5" id="productInfo">
               <p className="font-bold">{data.category}</p>
               <p className="italic">{data.description}</p>
-              <div className="flex items-center justify-between lg:gap-8 mt-8">
+              <div className="flex items-center justify-between lg:justify-start lg:gap-8 mt-8">
                 <p className="text-gray-500 font-bold text-lg">
                   {data.price} €
                 </p>
@@ -39,7 +41,7 @@ const ItemDetailView = ({ data }) => {
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
