1fe132d80f813a6a2ca958f3be498315c4efe0dc
Anna Esteve <aesteve8@gmail.com>
2023-08-22 11:08:21 +0200

    styles on safari changed

--- src/components/ItemGridView.js
+++ src/components/ItemGridView.js
@@ -10,7 +10,7 @@ const ItemGridView = ({ imgSrc, imgAlt, name, price, id }) => {
     <div className="w-full md:w-1/2 lg:w-1/3 lg:p-6 box-border hover:scale-110 transition duration-300 ease-in-out itemsContainer">
       <div className="bg-soft-grey w-full h-[400px] p-5 box-border flex justify-center items-center">
         <Link to={`/item/${id}`}>
-          <img className="max-h-full mx-auto" src={imgSrc} alt={imgAlt} />
+          <img className="max-h-[360px] mx-auto" src={imgSrc} alt={imgAlt} />
         </Link>
       </div>
 
