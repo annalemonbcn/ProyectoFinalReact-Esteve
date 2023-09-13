a0e53b6fe76d2b221c585b8bfba5b0363f24fcb9
Anna Esteve <aesteve8@gmail.com>
2023-09-13 20:42:21 +0200

    ProductsProvider updated

--- src/api/context/ProductsProvider.js
+++ src/api/context/ProductsProvider.js
@@ -1,16 +1,57 @@
 // Services
-import { fetchProducts } from "../services/apiService"
+import { fetchProductsFromFirestore } from "../services/firebaseService"
+
+// db
+import { db } from "../../db/firebase";
+
+// Firestore
+import { collection, query, where } from "firebase/firestore";
+
 
 // Context
-import { createContext, useState, useEffect } from "react"
+import { createContext, useState, useEffect, useParams } from "react"
 export const ProductsContext = createContext();
 
 
-// Provider
-const ProductsProvider = (props) => {
+// Provider for api
+// const ProductsProvider = (props) => {
 
+//   // State
+//   const [products, setProducts] = useState([]);
+
+//   // Effects
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   // useEffect(() => {
+//   //   console.log("Updated products:", products);
+//   // }, [products]); // --> demuestra que products tiene valores
+  
+//   // Actions
+//   const fetchData = async () => {
+//     try {
+//       const data = await fetchProducts();
+//       setProducts(data);
+//     } catch (error) {
+//       console.error("Error fetching products:", error);
+//     }
+//   };
+
+
+//   return(
+//     <ProductsContext.Provider value={products}>
+//       {props.children}
+//     </ProductsContext.Provider>
+//   )
+// }
+
+const ProductsProvider = (props) => {
   // State
   const [products, setProducts] = useState([]);
+
+  // Params
+  const params = useParams();  
 
   // Effects
   useEffect(() => {
@@ -23,8 +64,24 @@ const ProductsProvider = (props) => {
   
   // Actions
   const fetchData = async () => {
+    // Collection
+    const picturesCollection = collection(db, "pictures");
+
+    // Query to filter the products
+    let productsQuery;
+
+    // Set query if params
+    if (params.id) {
+      // If 'id' param in the URL, apply a filter
+      productsQuery = query(picturesCollection, where('size', '==', params.id));
+    } else {
+      // If no 'id' param in the URL, get all the products
+      productsQuery = picturesCollection;
+    }
+
+    // Fetch products from firestore
     try {
-      const data = await fetchProducts();
+      const data = await fetchProductsFromFirestore(productsQuery);
       setProducts(data);
     } catch (error) {
       console.error("Error fetching products:", error);
