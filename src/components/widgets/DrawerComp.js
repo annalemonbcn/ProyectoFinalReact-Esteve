e036d0fd04c8c1d230d732aaf581783942a727c3
Anna Esteve <aesteve8@gmail.com>
2023-09-26 17:24:31 +0200

    drawer working

--- src/components/widgets/DrawerComp.js
+++ src/components/widgets/DrawerComp.js
@@ -8,14 +8,13 @@ import CheckRoundIcon from "@rsuite/icons/CheckRound";
 // Context
 import { CartContext } from "../../api/context/CartProvider";
 
-
 const DrawerComp = ({ open, setIsDrawerOpen }) => {
   const size = "xs";
 
   const { cartItems, goToCart } = useContext(CartContext);
- 
+
   useEffect(() => {
-    console.log('cartItems:', cartItems)
+    console.log("cartItems:", cartItems);
   }, [cartItems]);
 
   const subtotal = cartItems.reduce(
@@ -43,27 +42,38 @@ const DrawerComp = ({ open, setIsDrawerOpen }) => {
             >
               View cart
             </button>
-            <button className="border-slate-500 border-solid border py-1 px-2 rounded hover:bg-slate-50">
+            <button
+              className="border-slate-500 border-solid border py-1 px-2 rounded hover:bg-slate-50"
+              onClick={() => setIsDrawerOpen(false)}
+            >
               Keep looking
             </button>
           </div>
           {/* Product list */}
-          <p className="font-bold border-b border-slate-500 mt-10">Cart summary</p>
+          <p className="font-bold border-b border-slate-500 mt-10">
+            Cart summary
+          </p>
           <ul className="max-w-full mt-4">
             {cartItems.map((item, i) => (
               <li className="flex gap-2 items-center mt-3" key={i}>
-                <img src={item.image} alt="item.title" className="max-w-[40%]" />
+                <img
+                  src={item.image}
+                  alt="item.title"
+                  className="max-w-[40%]"
+                />
                 <div>
                   <p className="text-sm font-bold">{item.title}</p>
                   <p className="text-sm italic">Quantity:{item.qty}</p>
-                  <p className="text-sm italic">Price: {item.price} €</p>
-                </div> 
+                  <p className="text-sm italic text-gray-500">
+                    Price: {item.price} €
+                  </p>
+                </div>
               </li>
             ))}
           </ul>
           {/* Total */}
           <div className="mt-8">
-            <p className="font-bold">Subtotal: {subtotal} €</p>
+            <p className="font-bold ">Subtotal: {subtotal} €</p>
           </div>
         </Drawer.Body>
       </Drawer>
