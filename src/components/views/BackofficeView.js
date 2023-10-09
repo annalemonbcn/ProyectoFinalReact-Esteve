b673e2df3c6320e58dd4f7b74d82b70af9838d6e
Anna Esteve <aesteve8@gmail.com>
2023-10-09 13:20:38 +0200

    mobile and ipad view implemented

--- src/components/views/BackofficeView.js
+++ src/components/views/BackofficeView.js
@@ -16,11 +16,18 @@ const BackofficeView = ({ orders }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [data, setData] = useState([]);
 
+  // Table values
+  const columns = [
+    { title: "Order ID", align: "text-center md:text-left" },
+    { title: "Date", align: "hidden md:block text-center md:text-left" },
+    { title: "User", align: "text-center md:text-left" },
+    { title: "Seen", align: "text-center md:text-left" },
+  ];
+
   useEffect(() => {
     if (orders.length > 0) {
       setData(orders);
     }
-    console.log("orders", orders);
   }, [orders]);
 
   // Modify order
@@ -58,36 +65,48 @@ const BackofficeView = ({ orders }) => {
         <Loader content="Loading orders..." />
       ) : (
         <>
-          <main className="px-5 xl:px-20 mb-16 lg:mb-4 backoffice">
+          <main className="px-5 xl:px-48 mb-16 lg:mb-4 backoffice">
             <h1 className="text-center text-2xl md:text-3xl my-8 px-5">
               Orders
             </h1>
-            <div className="grid grid-cols-4 gap-4">
-              <div className="bg-gray-200 p-2">Order ID</div>
-              <div className="bg-gray-200 p-2">Date</div>
-              <div className="bg-gray-200 p-2">User</div>
-              <div className="bg-gray-200 p-2">Seen</div>
+            <div className="grid grid-cols-3 md:grid-cols-4 gap-4">
+              {columns.map((column, i) => (
+                <div key={i} className={`bg-gray-200 p-2 ${column.align}`}>
+                  {column.title}
+                </div>
+              ))}
               {data.map((order, i) => {
+                const cells = [
+                  { value: order.id, align: "text-center md:text-left" },
+                  {
+                    value: convertTimestamp(order.date),
+                    align: "hidden md:block text-center md:text-left",
+                  },
+                  {
+                    value: order.user.email,
+                    align: "text-center md:text-left",
+                  },
+                  {
+                    value: order.seen ? "✅" : "❌",
+                    align: "text-center md:text-left",
+                  },
+                ];
                 return (
                   <React.Fragment key={i}>
-                    <div
-                      className="p-2 cursor-pointer"
-                      onClick={() => handleOrderClick(order)}
-                    >
-                      {order.id}
-                    </div>
-                    <div className="p-2 cursor-pointer">
-                      {convertTimestamp(order.date)}
-                    </div>
-                    <div className="p-2 cursor-pointer">{order.user.email}</div>
-                    <div className="p-2 cursor-pointer">
-                      {order.seen ? "✅" : "❌"}
-                    </div>
+                    {cells.map((cell, cellIndex) => (
+                      <div
+                        key={cellIndex}
+                        className={`p-2 cursor-pointer overflow-auto ${cell.align}`}
+                        onClick={() => handleOrderClick(order)}
+                      >
+                        {cell.value}
+                      </div>
+                    ))}
                   </React.Fragment>
                 );
               })}
             </div>
-            <div className="mt-16 flex justify-end pr-8">
+            <div className="mt-16 flex justify-end md:pr-8">
               <NavLink
                 to="/"
                 className="px-5 py-2.5 bg-black text-white font-bold"
